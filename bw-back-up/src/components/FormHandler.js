ea28843587a33d4f940de687fd4a8e1fbe93c541
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-29 14:37:55 -0700

    update.

--- bw-back-up/src/components/FormHandler.js
+++ bw-back-up/src/components/FormHandler.js
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
 
-function Form(props) {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
-export default Form
+// function PostUser(props) {
+//     return (
+//         const postuser = user => {
+//     axios.post(props.url, user)
+//         .then(res => {
+//             setUsers([...users, res.data])
+//             console.log(res)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//             debugger
+//         })
+// })}
+// export default PostUser
